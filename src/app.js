47694f4bcaa1d542c306c19ab884cac756f4c7c5
Craveiro <henrique.cne@gmail.com>
2017-02-03 03:03:47 -0200

    Salvando no banco de dados

--- src/app.js
+++ src/app.js
@@ -15,19 +15,78 @@
     sendImage(canvas.toDataURL('image/jpeg'))
   }
 
-  var sendImage = function(image){
+  var makeblob = function (dataURL) {
+      var BASE64_MARKER = ';base64,';
+      if (dataURL.indexOf(BASE64_MARKER) == -1) {
+          var parts = dataURL.split(',');
+          var contentType = parts[0].split(':')[1];
+          var raw = decodeURIComponent(parts[1]);
+          return new Blob([raw], { type: contentType });
+      }
+      var parts = dataURL.split(BASE64_MARKER);
+      var contentType = parts[0].split(':')[1];
+      var raw = window.atob(parts[1]);
+      var rawLength = raw.length;
 
+      var uInt8Array = new Uint8Array(rawLength);
+
+      for (var i = 0; i < rawLength; ++i) {
+          uInt8Array[i] = raw.charCodeAt(i);
+      }
+
+      return new Blob([uInt8Array], { type: contentType });
+  }
+
+  var sendImage = function(image){
+    var imageOctet = makeblob(image);
     $.ajax({
       type: "POST",
+      processData: false,
       url: 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
-      data: JSON.stringify({"url": "http://www.marcandangel.com/images/9-not-need-happy.jpg"}),
+      // data: JSON.stringify({"url": "http://www.marcandangel.com/images/9-not-need-happy.jpg"}),
+      data: imageOctet,
       success: function(result){
-        console.log("Deu certo", result[0].scores);
+        saveDb(result);
       },
       headers: {
         'Ocp-Apim-Subscription-Key': '8a6b4adcde9f4bae98be707d36d14bc2',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/octet-stream'
 
+      }
+    });
+
+  }
+
+
+  var saveDb = function(alunos){
+    alunos = alunos.reduce(function(current, aluno, index){
+        current.push({
+          aluno: 'ID'+ index,
+          data: new Date(),
+          anger: aluno.scores.anger,
+          contempt: aluno.scores.contempt,
+          disgust: aluno.scores.disgust,
+          fear: aluno.scores.fear,
+          happiness: aluno.scores.happiness,
+          neutral: aluno.scores.neutral,
+          sadness: aluno.scores.sadness,
+          surprise: aluno.scores.surprise,
+        })
+        return current;
+    }, [])
+
+    console.log(alunos);
+
+    $.ajax({
+      type: "POST",
+      url: 'https://api.mlab.com/api/1/databases/thebighackathon/collections/alunos?apiKey=iXLuxajzftf_48QqNCa1j-EA6ywBBfMc',
+      data: JSON.stringify(alunos),
+      success: function(result){
+        console.log("Deu certo", result);
+
+      },
+      headers: {
+        'Content-Type': 'application/json'
       }
     });
 
