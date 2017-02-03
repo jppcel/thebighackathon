972392b96a481ae67e3b1de0e13b308b0c84a472
João Paulo Polles <joaopaulopolles@gmail.com>
2017-02-03 15:59:12 -0200

    fim

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.use(express.static(__dirname+'/public'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+app.get('/', function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+app.get('/painel/', function(req,res){
+  res.sendFile(__dirname+'/painel/index.html');
+});
+app.get('/painel/alunos', function(req,res){
+  res.sendFile(__dirname+'/painel/alunos.html');
+});
+app.get('/painel/alunos/view/1', function(req,res){
+  res.sendFile(__dirname+'/painel/historicoAluno.html');
+});
