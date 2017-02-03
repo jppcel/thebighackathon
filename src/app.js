3ac8cf506928b111ea5146558353f37bb14b8b68
Craveiro <henrique.cne@gmail.com>
2017-02-03 02:03:31 -0200

    Enviando imagem para azure

--- src/app.js
+++ src/app.js
@@ -8,17 +8,29 @@
   canvas.setAttribute('width', width);
   canvas.setAttribute('height', height);
 
-  var takePhoto = function(){
+  var takeImage = function(){
     ctx.drawImage(video, 0, 0, width, height);
     image.src = canvas.toDataURL('image/png');
-    var request = new Request('http://localhost:3000/sendphoto', {method: 'POST', body: '{"foo":"bar"}'});
-    fetch(request).then(function(success){
-      console.log(success)
-    })
-    .catch(function(error){
-      console.log("Deu merda:", error)
-    })
     // console.log("Tirou foto", canvas.toDataURL('image/jpg'));
+    sendImage(canvas.toDataURL('image/jpeg'))
+  }
+
+  var sendImage = function(image){
+
+    $.ajax({
+      type: "POST",
+      url: 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
+      data: JSON.stringify({"url": "http://www.marcandangel.com/images/9-not-need-happy.jpg"}),
+      success: function(result){
+        console.log("Deu certo", result[0].scores);
+      },
+      headers: {
+        'Ocp-Apim-Subscription-Key': '8a6b4adcde9f4bae98be707d36d14bc2',
+        'Content-Type': 'application/json'
+
+      }
+    });
+
   }
 
   navigator
@@ -27,7 +39,7 @@
     .then(function(localMediaStream) {
       video.src = window.URL.createObjectURL(localMediaStream);
       setInterval(function(){
-        takePhoto();
+        takeImage();
       }, 10000);
     })
     .catch(function(error){
